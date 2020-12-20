63d04a796eaea6b7d68029553e35ac91e02b5706
Romain Charpentier <romain.charpentier10@gmail.com>
2020-12-20 16:02:49 +0100

    loading interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs/operators';
+import { LoadingService } from './shared/services/loading.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AngularBlog';
+export class AppComponent implements OnInit {
+  title = 'angular-boilerplate';
+  loading = false;
+
+  constructor(
+    private loadingService: LoadingService
+  ){ }
+
+  ngOnInit(): void{
+    this.listenToLoading();
+  }
+
+  /**
+   * Listen to the loadingSub property in the LoadingService class. This drives the
+   * display of the loading spinner.
+   */
+  listenToLoading(): void {
+    this.loadingService.loadingSub
+      .pipe(delay(0)) // This prevents a ExpressionChangedAfterItHasBeenCheckedError for subsequent requests
+      .subscribe((value) => {
+        this.loading = value;
+      });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostItemComponent } from './post/post-item/post-item.component';
 import { PostListComponent } from './post/post-list/post-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpRequestInterceptor } from './interceptors/http-request-interceptor';
 
 @NgModule({
   declarations: [
@@ -22,9 +24,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatCardModule, MatButtonModule, MatIconModule
+    MatCardModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
